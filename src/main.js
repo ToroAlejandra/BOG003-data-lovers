7e1e79d32cf484186a932c234d9409a0aff28df3
Diana Buitrago <dianabuitrago54@hotmail.es>
2021-07-15 15:30:17 -0500

    Se anexan div para las tarjetas

--- src/main.js
+++ src/main.js
@@ -5,36 +5,51 @@ import data from './data/pokemon/pokemon.js';
 
 console.log(example, data);
 
-console.log(data.pokemon[0].num);
-console.log(data.pokemon[0].size.height);
+//console.log(data.pokemon[0].num);
+//console.log(data.pokemon[0].size.height);
 
-
-for (let i = 0; i <data.pokemon.length; i++) {
-   
-    console.log(i);
-    //Se crean los contenedores de cada tarjeta
+data.pokemon.forEach((element) => {
     let box = document.createElement("div");
-    box.setAttribute("class", "boxPokemon");
+    let boxCard = document.createElement("div");
+    let nameBox = document.createElement("div");
+    let imgPokemon = document.createElement("img");
+    let numPokemon = document.createElement("h2");
+    let namePokemon = document.createElement("h2");
+    let containerBox = document.getElementById("container");
+
+    //Se establece el atributo para el estilo de los div
+    containerBox.setAttribute("class", "containerCard");
+    boxCard.setAttribute("class", "boxPokemon");
+    nameBox.setAttribute("class", "name-NumPokemon");
 
     //Se toma la imagen de la data y se añade al index
-    let imgPokemon = document.createElement("img");
-    imgPokemon.src = data.pokemon[i].img;
+    imgPokemon.src = element.img;
     box.appendChild(imgPokemon);
 
     //Se toma el número del pokémon de la data y se añade al index
-    let numPokemon = document.createElement("h2");
-    numPokemon.innerHTML = data.pokemon[i].num;
-    box.appendChild(numPokemon);
+    numPokemon.innerHTML = element.num;
+    nameBox.appendChild(numPokemon);
 
     //Se toma el nombre del pokémon de la data y se añade al index
-    let namePokemon = document.createElement("h2");
-    namePokemon.innerHTML = data.pokemon[i].name;
-    box.appendChild(namePokemon);
+    namePokemon.innerHTML = element.name;
+    nameBox.appendChild(namePokemon);
 
-    let containerBox = document.getElementById("container");
-    containerBox.setAttribute("class", "containerCard");
+    box.appendChild(boxCard);
+    box.appendChild(nameBox);
+    
+    
+    
 
     containerBox.appendChild(box);
+
+
+});
+
+for (let i = 0; i < data.pokemon.length; i++) {
+
+    //console.log(i);
+    //Se crean los contenedores de cada tarjeta
+
 }
 
 
@@ -48,21 +63,21 @@ for (let i = 0; i <data.pokemon.length; i++) {
 /*for (let i = 0; i < data.pokemon.length; i++) {
     let cardPokemon = document.createElement("SECTION");
     cardPokemon.setAttribute("class", "item");
-    
+
     let imgPokemon = document.createElement("IMG");
     imgPokemon.src = data.pokemon[i].img;
-    cardPokemon.appendChild(imgPokemon);  
+    cardPokemon.appendChild(imgPokemon);
 
     let namePokemon = document.createElement("h4");
     namePokemon.innerHTML = data.pokemon[i].name;
-    cardPokemon.appendChild(namePokemon); 
+    cardPokemon.appendChild(namePokemon);
 
     let numPokemon = document.createElement("h3");
     numPokemon.innerHTML = data.pokemon[i].num;
-    cardPokemon.appendChild(numPokemon); 
-    
+    cardPokemon.appendChild(numPokemon);
+
     cardPokemon.insertBefore(numPokemon, namePokemon);
-    
+
     document.getElementById("cards").appendChild(cardPokemon);
 }
 */

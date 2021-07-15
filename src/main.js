27f1eb0e16ab5e0595746a4eb0f8a6e7692a4eaf
Diana Buitrago <dianabuitrago54@hotmail.es>
2021-07-15 09:29:33 -0500

    Se realizaron los div y se da estilos con grid

--- src/main.js
+++ src/main.js
@@ -8,29 +8,33 @@ console.log(example, data);
 console.log(data.pokemon[0].num);
 console.log(data.pokemon[0].size.height);
 
-let card = [];
-//let idCard = "";
 
-for (let i = 0; i < data.pokemon.length; i++) {
-    let idCard = "card"+ i.toString();
-    card[i] = document.createElement("section");
-    card[i].setAttribute("id", i);
+for (let i = 0; i <data.pokemon.length; i++) {
+   
     console.log(i);
+    //Se crean los contenedores de cada tarjeta
+    let box = document.createElement("div");
+    box.setAttribute("class", "boxPokemon");
+
     //Se toma la imagen de la data y se añade al index
     let imgPokemon = document.createElement("img");
     imgPokemon.src = data.pokemon[i].img;
-    document.getElementById(i).appendChild(imgPokemon);
+    box.appendChild(imgPokemon);
 
     //Se toma el número del pokémon de la data y se añade al index
     let numPokemon = document.createElement("h2");
     numPokemon.innerHTML = data.pokemon[i].num;
-    document.getElementById(i).appendChild(numPokemon);
+    box.appendChild(numPokemon);
 
     //Se toma el nombre del pokémon de la data y se añade al index
     let namePokemon = document.createElement("h2");
     namePokemon.innerHTML = data.pokemon[i].name;
-    document.getElementById(i).appendChild(namePokemon);
-    document.getElementById("cards").appendChild(card[i]);    
+    box.appendChild(namePokemon);
+
+    let containerBox = document.getElementById("container");
+    containerBox.setAttribute("class", "containerCard");
+
+    containerBox.appendChild(box);
 }
 
 
